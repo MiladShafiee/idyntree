b7d9106493d60961e019053f17a2151a942d6508
traversaro <pegua1@gmail.com>
2020-01-29 09:01:45 +0000

    Deploying to gh-pages from master 4427d95d3f884d9ab829777c350508554bfda789

---
+++ devel/navtreedata.js
@@ -0,0 +1,82 @@
+var NAVTREE =
+[
+  [ "iDynTree", "index.html", [
+    [ "Welcome to iDynTree", "index.html", null ],
+    [ "iDynTree ChangeLog", "changelog.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Properties", "functions_prop.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"AccelerometerSensor_8cpp.html",
+"EigenHelpers_8h.html#a3d1f28b899ee110b8af0c92c9945455c",
+"LinkState_8cpp.html",
+"TestUtils_8cpp.html#a293fa271f52f72d795f4858b34273ed6",
+"YARPConversions_8h.html#ga63598edaacfc53e5728e944a0dab5ebb",
+"classObserverThread.html#a37796c9a53d7e074b82c28eeb2b123d8",
+"classiDynTree_1_1AttitudeMahonyFilter.html#ab40c1b03d4acc60dcdf9dc9dcdb6c9d4",
+"classiDynTree_1_1ClassicalAcc.html#a9ce69cc1e892338d4e2d0b5c13ebbedf",
+"classiDynTree_1_1DummyModelVisualization.html#a63993e4f1e77021cda48a5d6ea772a38",
+"classiDynTree_1_1FreeFloatingPos.html#ad887c07a7b8d48452680e9ba14529c42",
+"classiDynTree_1_1IVectorsVisualization.html#ac09f7374d877e8ded950f659542fb811",
+"classiDynTree_1_1KinDynComputations.html#a9ce824b898ed0988641c4e28072980d4",
+"classiDynTree_1_1MatrixDynSize.html#a062017b1f0958fa8d18edd822ad7018f",
+"classiDynTree_1_1MotionVector3.html#aab92d8d49cafbb94d6ced67ffc514910",
+"classiDynTree_1_1Regressors_1_1DynamicsRegressorGenerator.html#a6fd4851eb58b915f0e052d4a165c6ffa",
+"classiDynTree_1_1RotationalInertiaRaw.html#a58750c21e5e8c31c54c3eed7dd2309b6",
+"classiDynTree_1_1Span.html#a373be1ad1db1e1f4c8ebd87d838c0dc6",
+"classiDynTree_1_1SpatialInertia.html#ac8f2cb43371716b2aeb3a893e4e14462",
+"classiDynTree_1_1ThreeAxisForceTorqueContactSensor.html#aff3b2cd0b1a847dc6336c55042131e6e",
+"classiDynTree_1_1VectorFixSize.html#ada7487eff1fee83db8d41165d30f1263",
+"classiDynTree_1_1optimalcontrol_1_1ConstraintsGroup.html#a116fdd0435e3dd18526eaef723d021ca",
+"classiDynTree_1_1optimalcontrol_1_1LinearCost.html#a6f758bc67249991beddf14886bd646d4",
+"classiDynTree_1_1optimalcontrol_1_1OptimalControlProblem_1_1OptimalControlProblemPimpl.html#a9c2d6c978e6f2d7511b3779dc875f16a",
+"classiDynTree_1_1optimalcontrol_1_1integrators_1_1IntegratorInfo.html#a09f6bc112ba387fa190f7498a3c63d72",
+"classiDynTree_1_1optimization_1_1OsqpInterface_1_1OsqpInterfaceImplementation.html#a8dc6074d50228c61fb791418bb866627",
+"config_8h.html#a241a44dc00df371b6342f3232d1867cb",
+"iCub_2skinDynLibConversions_8h.html",
+"structiDynTree_1_1FTSensorData.html#a8258fc3e60afe956afda6daac2ce5d92",
+"structiDynTree_1_1optimalcontrol_1_1BufferedGroup.html#aef019aced8060f89f352ab76980d68b0"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
